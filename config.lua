62fa5131b430571271dbac33fc79484635461352
Slewog <31140215+Slewog@users.noreply.github.com>
2019-01-24 00:45:26 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+Config.DrawDistance = 100
+Config.Size         = {x = 1.3, y = 1.3, z = 0.5}
+Config.Color        = {r = 0, g = 204, b = 3}
+Config.Type         = 1
+Config.Locale = 'fr'
+
+Config.Zones = {
+
+    SmokeOnTheWater = {
+        Items = {},
+        Pos = {
+            {x = -1172.355,   y = -1571.750,  z = 3.660},         
+        }
+    }
+}
---
+++ locales/fr.lua
@@ -0,0 +1,9 @@
+Locales['fr'] = {
+
+	['weedshop'] = 'Smoke on the water',
+	['weedshops'] = 'Smoke on the water',
+	['press_menu3'] = 'appuyez sur ~INPUT_CONTEXT~ pour accéder au magasin.',
+	['bought'] = 'vous avez acheté ~b~1x ',
+	['not_enough'] = 'vous n\'avez ~r~pas assez~s~ d\'argent.',
+
+}

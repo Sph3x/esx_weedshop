bcdf0792671be7e2f4488aefba63532a24029f09
Slewog <31140215+Slewog@users.noreply.github.com>
2019-06-19 03:03:43 +0200

    Update fr.lua

--- locales/fr.lua
+++ locales/fr.lua
@@ -1,9 +1,29 @@
 Locales['fr'] = {
 
-	['weedshop'] = 'Smoke on the water',
-	['weedshops'] = 'Smoke on the water',
-	['press_menu3'] = 'appuyez sur ~INPUT_CONTEXT~ pour accéder au magasin.',
-	['bought'] = 'vous avez acheté ~b~1x ',
-	['not_enough'] = 'vous n\'avez ~r~pas assez~s~ d\'argent.',
+    ['xanax']               = 'CBD',
+	['weed1']               = 'Cannabis Médicinal',
+    ['weed2']               = 'Résinne Médicinal',
+	['weed3']               = 'THC',
+	['weed4']               = 'Amnésia',
+	['weed5']               = 'Blue Cheese',
+	['weed6']               = 'Purple Haze',
+	['weed7']               = 'Orange Bud',
+	['weed8']               = 'White Widow',
+	['weed9']               = 'Bhuda Cheese',
+	['weed10']              = 'Royal roulé',
+	['weed11']              = 'Afghan Polm',
+	['weed12']              = 'Ketama Gold',
+	['weed13']              = 'Caramello',
+	['weed14']              = 'Hia',
+	['weed15']              = 'King Hassan',
+	
+	['bong']                = 'Bang',
+	['yes']                 = 'Oui',
+    ['no']                  = 'Non',
+    ['press_menu']          = 'Appuyez sur ~INPUT_CONTEXT~ pour accéder au Weed Shop',
+    ['drugstore']           = 'Smoke On The Water',
+    ['valid_this_purchase'] = 'Valider votre achat',
+    ['not_enough_money']    = 'Vous n\'avez pas assez d\'argent',
+    ['bought']              = 'Vous avez acheté un joint de ~g~'
 
 }

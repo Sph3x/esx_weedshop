0596f67159f0d75ff47da9ea7475fea40f7173e9
Slewog <31140215+Slewog@users.noreply.github.com>
2019-06-19 03:02:57 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,17 +1,38 @@
-Config = {}
-
-Config.DrawDistance = 100
-Config.Size         = {x = 1.3, y = 1.3, z = 0.5}
-Config.Color        = {r = 0, g = 204, b = 3}
-Config.Type         = 1
+Config              = {}
+Config.DrawDistance = 100.0
+Config.MarkerSize   = { x = 1.5, y = 1.5, z = 1.0 }
+Config.MarkerColor  = { r = 0, g = 204, b = 3 }
+Config.MarkerType   = 1
+Config.BlipSprite   = 140
+Config.BlipColour   = 24
+Config.ZDiff        = 0.5
 Config.Locale = 'fr'
 
-Config.Zones = {
+Config.Shops = {
+    
+    [1] = { x = -1172.355,   y = -1571.750,  z = 3.660 }
 
-    SmokeOnTheWater = {
-        Items = {},
-        Pos = {
-            {x = -1172.355,   y = -1571.750,  z = 3.660},         
-        }
-    }
+}
+
+Config.Price = {
+
+    ['xanax']    = 5.0,
+	['weed1']    = 15.0,
+    ['weed2']    = 20.0,
+	['weed3']    = 25.0,
+	['weed4']    = 13.0,
+	['weed5']    = 14.0,
+	['weed6']    = 16.0,
+	['weed7']    = 11.0,
+	['weed8']    = 10.0,
+	['weed9']    = 15.0,
+	['weed10']   = 12.0,
+	['weed11']   = 5.0,
+	['weed12']   = 7.0,
+	['weed13']   = 8.0,
+	['weed14']   = 10.0,
+	['weed15']   = 11.0,
+	['bong']     = 25.0
+	
+
 }

0949e65097d052910d84c37024fca85be3c4d5eb
Slewog <31140215+Slewog@users.noreply.github.com>
2019-06-21 01:24:39 +0200

    add english translate

---
+++ locales/en.lua
@@ -0,0 +1,29 @@
+Locales['en'] = {
+
+    ['xanax']               = 'CBD',
+	['weed1']               = 'Medicinal Cannabis',
+    ['weed2']               = 'Resinne Medicinal',
+	['weed3']               = 'THC',
+	['weed4']               = 'Amnésia',
+	['weed5']               = 'Blue Cheese',
+	['weed6']               = 'Purple Haze',
+	['weed7']               = 'Orange Bud',
+	['weed8']               = 'White Widow',
+	['weed9']               = 'Bhuda Cheese',
+	['weed10']              = 'Royal roulé',
+	['weed11']              = 'Afghan Polm',
+	['weed12']              = 'Ketama Gold',
+	['weed13']              = 'Caramello',
+	['weed14']              = 'Hia',
+	['weed15']              = 'King Hassan',
+	
+	['bong']                = 'Bang',
+	['yes']                 = 'Yes',
+    ['no']                  = 'No',
+    ['press_menu']          = 'Press ~INPUT_CONTEXT~ to access the Weed Shop',
+    ['drugstore']           = 'Smoke On The Water',
+    ['valid_this_purchase'] = 'valid this purchase',
+    ['not_enough_money']    = 'not enough money',
+    ['bought']              = 'You bought a seal of ~g~'
+
+}
